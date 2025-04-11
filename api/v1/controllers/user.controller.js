603b40c9db076965c7acf7cb63c127b1a81b91fc
chison2707 <chiphansonzz17@gmail.com>
2025-04-11 16:27:59 +0700

    login

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -3,6 +3,7 @@ const User = require('../models/user.model.js');
 const saltRounds = 10;
 const generateHelper = require('../helpers/generate.js');
 
+// [POST]/api/v1/users/register
 module.exports.register = async (req, res) => {
     const { full_name, email, password } = req.body;
     const EsistUser = await User.findByEmail(email);
@@ -20,5 +21,32 @@ module.exports.register = async (req, res) => {
     return res.json({
         code: 200,
         message: 'Đăng ký thành công'
+    });
+}
+
+// [GET]/api/v1/users/login
+module.exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findByEmail(email);
+    if (!user) {
+        return res.json({
+            code: 400,
+            message: 'Email không tồn tại'
+        });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.json({
+            code: 400,
+            message: 'Mật khẩu không chính xác'
+        });
+    }
+    const token = user.token;
+    res.cookie("token", token);
+
+    return res.json({
+        code: 200,
+        message: 'Đăng nhập thành công',
+        token: token,
     });
 }
--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controller = require("../controllers/user.controller");
 
 router.post('/register', controller.register);
+router.get('/login', controller.login);
 
 module.exports = router;

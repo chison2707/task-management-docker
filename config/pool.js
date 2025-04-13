0226162b408529c2ab9ac9bd545e0dd41f85b2a0
chison2707 <chiphansonzz17@gmail.com>
2025-04-13 15:31:25 +0700

    fix env

--- config/pool.js
+++ config/pool.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg');
+require("dotenv").config();
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'task-management',
-    password: '123456',
+    user: process.env.DATABASE_USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.DATABASE_PASS,
     port: 5432
 });
 
